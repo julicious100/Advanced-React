4563db65baab33a35e3efc5a8df988ba1a5d0269
Julie Beak <beakjulie@gmail.com>
2020-05-18 14:23:24 -0700

    added cookie funtion on backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,5 +1,6 @@
 // This is the entry point of the application
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -7,8 +8,19 @@ const db = require("./db");
 
 const server = createServer();
 
-// TODO: Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser()); // accept all the cookies readable
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  // console.log("Heyyyy this is middleware");
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 // TODO: Use express middleware to populate current user
 
